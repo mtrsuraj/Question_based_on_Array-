468b5900a38afcd4d205e12ec1eac363e05fc6f2
mtr_suraj <89896051+surajrepository@users.noreply.github.com>
2022-06-08 01:14:19 +0530

    Add files via upload

---
+++ 2nd_method_to_reverse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+  for(int i=0, j=n-1; i<=n/2; i++,j--){
+            int temp=0;
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+           
+    }
+
+     for(int i=0; i<n; i++)
+        cout<<arr[i];
+    return 0;
+}
